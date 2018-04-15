e7249ec151ced9b1ef0a77d169758ac46a03123a
Alyssa Holzer <holzealy@uw.edu>
2018-04-14 17:26:08 -0700

    got counts working

--- aggregate.py
+++ aggregate.py
@@ -1,10 +1,12 @@
 import pandas as pd
+import numpy as np
 
 def main():
     conditions = pd.read_csv("final.csv")
+    conditions = conditions.apply(lambda x: x.astype(str).str.lower())
     conditions = conditions.sort_values("diagnosis")
-# 
-#counts = conditions.value_counts()
+    counts = conditions.diagnosis.value_counts()
+    counts.to_csv('counts.csv')
 
 if __name__ == "__main__":
     main()
